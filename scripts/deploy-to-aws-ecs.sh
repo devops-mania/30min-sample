7122af7ce0d66f7ccc885fdae29ab094a61f1fda
二木 隆彰 <takaaki.futatsugi@unisys.co.jp>
2018-05-09 16:07:08 +0900

    プッシュしたイメージを起動する設定を追加

--- scripts/deploy-to-aws-ecs.sh
+++ scripts/deploy-to-aws-ecs.sh
@@ -9,3 +9,18 @@ $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 
 docker tag test:latest "$imageName"
 docker push "$imageName"
+
+task=$(printf '[{
+  "name": "30min-sample-container",
+  "image": "%s",
+  "essential": true,
+  "memory": 300,
+  "cpu": 10,
+  "portMappings": [
+    { "hostPort": 80, "protocol": "tcp", "containerPort": 3000 }
+  ]
+}]' $imageName)
+
+taskDefResponse=$(aws ecs register-task-definition --container-definitions "$task" --family "30min-sample-task")
+taskArn=$(echo $taskDefResponse | sed s/\"/\\n/g | grep ^arn:aws:ecs:)
+aws ecs update-service --cluster $ECS_CLUSTER --service $ECS_SERVICE --task-definition $taskArn
